fa341a054d4e7d9f0284c290d6e835a0279a525e
ckouta <ckouta@LAPTOP-3U0F2GJ4>
2019-04-16 15:33:32 -0300

    prueba de rama

--- src/main/java/com/barberia/springboot/app/controllers/IndexController.java
+++ src/main/java/com/barberia/springboot/app/controllers/IndexController.java
@@ -10,7 +10,7 @@ public class IndexController {
 	
 	@GetMapping("/")
 	public String hola(Model model) {
-		String mensaje="Proyecto Semestral";
+		String mensaje="Proyecto Semestral holla";
 		model.addAttribute("mensaje",mensaje);
 		return "index";
 	}
