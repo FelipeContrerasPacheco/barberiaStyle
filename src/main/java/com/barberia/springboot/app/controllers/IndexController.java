244a21f7937c79fd2794a876a4f3dd61ea7b0e85
jeorgeneftally <jeorgeceacadiz@gmail.com>
2019-04-16 14:22:50 -0400

    index

--- src/main/java/com/barberia/springboot/app/controllers/IndexController.java
+++ src/main/java/com/barberia/springboot/app/controllers/IndexController.java
@@ -10,7 +10,7 @@ public class IndexController {
 	
 	@GetMapping("/")
 	public String hola(Model model) {
-		String mensaje="Proyecto Semestral";
+		String mensaje="Proyecto Semestral de gestion";
 		model.addAttribute("mensaje",mensaje);
 		return "index";
 	}
