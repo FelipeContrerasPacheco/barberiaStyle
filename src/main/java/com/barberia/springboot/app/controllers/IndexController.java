1fd8a9de2094efa21f3e6e9d582b20a8947736aa
Acelera Bu <carlosfelipe.alarcon@gmail.com>
2019-05-28 19:12:18 -0400

    Merge branch 'Felipe' of https://github.com/FelipeContrerasPacheco/barberiaStyle into Carlos
    Conflicts:
    	src/main/java/com/barberia/springboot/app/controllers/barberoController.java
    	src/main/java/com/barberia/springboot/app/models/entity/Barbero.java

--- src/main/java/com/barberia/springboot/app/controllers/IndexController.java
+++ src/main/java/com/barberia/springboot/app/controllers/IndexController.java
@@ -5,12 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-
 public class IndexController {
 	
 	@GetMapping("/")
 	public String hola(Model model) {
-		String mensaje="Proyecto Semestral";
+		String mensaje="Proyecto Semestral de gestion";
 		model.addAttribute("mensaje",mensaje);
 		return "index";
 	}
--- src/main/java/com/barberia/springboot/app/models/entity/Barbero.java
+++ src/main/java/com/barberia/springboot/app/models/entity/Barbero.java
@@ -1,5 +1,6 @@
 package com.barberia.springboot.app.models.entity;
 
+<<<<<<< HEAD
 import java.io.Serializable;
 
 import javax.persistence.Entity;
@@ -13,6 +14,19 @@ import javax.validation.constraints.NotEmpty;
 @Entity
 @Table(name="barberos")
 public class Barbero implements Serializable {
+=======
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name="barberos")
+public class Barbero {
+>>>>>>> branch 'Felipe' of https://github.com/FelipeContrerasPacheco/barberiaStyle
 	
 	private static final long serialVersionUID=1L;
 	@Id
